a0cd1f12c69744adce1ffbc880cd18ddd3bb6ba8
EdwinYung <eyung7@gmail.com>
2017-12-18 14:57:33 -0500

    reservation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Nav from "./Nav";
-import Header from "./Header";
-import Welcome from "./Welcome";
-import Contact from "./Contact";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav';
+import Header from './Header';
+import Welcome from './Welcome';
+import Contact from './Contact';
+import Reservation from './Reservation';
 
 class App extends Component {
   render() {
