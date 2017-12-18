791080c4912787c6d51994103f0f18ad50987193
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-18 17:44:49 -0600

    about to blow up food menu items

--- src/FoodMenu.js
+++ src/FoodMenu.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React from "react";
+import DropDownMenu from "material-ui/DropDownMenu";
+import MenuItem from "material-ui/MenuItem";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 //import faker from 'faker';
+
+// const FoodItem = ({ food }) => {
+//   const link = "#" + title.toLowerCase();
+//   return (
+//     <li>
+//       <a href={link}>{title}</a>
+//     </li>
+//   );
+// };
 
 const FoodMenuItems = ({ foodItems }) => {
   return (
     <div>
-      <p>{foodItems.name}</p>
-      <p>{foodItems.description}</p>
-      <p>${foodItems.price}</p>
-      <p>***</p>
+      {foodItems.map(foodItem => {
+        return (
+          <div>
+            <p>{foodItem.name}</p>
+            <p>{foodItem.description}</p>
+            <p>${foodItem.price}</p>
+            <p>***</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -29,25 +44,25 @@ export default class DropDownMenuSimpleExample extends React.Component {
         <DropDownMenu value={this.state.value} onChange={this.handleChange}>
           <FoodMenuItems
             foodItems={[
-              { name: 'Filet', price: 50, description: 'lorem' },
-              { name: 'Lobster', price: 50, description: 'lorem' },
-              { name: 'Caviar', price: 100, description: 'lorem' }
+              { name: "Filet", price: 50, description: "lorem" },
+              { name: "Lobster", price: 50, description: "lorem" },
+              { name: "Caviar", price: 100, description: "lorem" }
             ]}
             primaryText="Chef's Choice"
           />
           <FoodMenuItems
             foodItems={[
-              { name: 'Shrimp', price: 50, description: 'lorem' },
-              { name: 'Rice', price: 50, description: 'lorem' },
-              { name: 'Chicken', price: 100, description: 'lorem' }
+              { name: "Shrimp", price: 50, description: "lorem" },
+              { name: "Rice", price: 50, description: "lorem" },
+              { name: "Chicken", price: 100, description: "lorem" }
             ]}
             primaryText="Dinner"
           />
           <FoodMenuItems
             foodItems={[
-              { name: 'Beer', price: 50, description: 'lorem' },
-              { name: 'Wine', price: 50, description: 'lorem' },
-              { name: 'OJ', price: 100, description: 'lorem' }
+              { name: "Beer", price: 50, description: "lorem" },
+              { name: "Wine", price: 50, description: "lorem" },
+              { name: "OJ", price: 100, description: "lorem" }
             ]}
             primaryText="Drink"
           />
